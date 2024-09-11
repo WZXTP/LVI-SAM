ca8c114bdf8465e667bf52998900c9f5da335114
WZXTP <145897967+WZXTP@users.noreply.github.com>
2024-09-11 21:41:15 +0800

    Update imu_factor.h

--- src/visual_odometry/visual_estimator/factor/imu_factor.h
+++ src/visual_odometry/visual_estimator/factor/imu_factor.h
@@ -8,9 +8,13 @@
 #include "integration_base.h"
 
 #include <ceres/ceres.h>
+//用于实现IMU测量的残差计算以及雅可比矩阵的推导。
 
 class IMUFactor : public ceres::SizedCostFunction<15, 7, 9, 7, 9>
 {
+//残差向量的维度是 15（IMU预积分中的位姿、速度、偏置等）。
+//4 组优化变量的维度分别为 7（姿态四元数+位置）、9（速度+加速度计和陀螺仪偏置）、7、9。
+
   public:
 
     IntegrationBase* pre_integration;
@@ -23,37 +27,42 @@ class IMUFactor : public ceres::SizedCostFunction<15, 7, 9, 7, 9>
     virtual bool Evaluate(double const *const *parameters, double *residuals, double **jacobians) const
     {
 
-        Eigen::Vector3d Pi(parameters[0][0], parameters[0][1], parameters[0][2]);
-        Eigen::Quaterniond Qi(parameters[0][6], parameters[0][3], parameters[0][4], parameters[0][5]);
+        //parameters[0] 对应i帧的位姿（位置和旋转）。
+        Eigen::Vector3d Pi(parameters[0][0], parameters[0][1], parameters[0][2]);//Pi 是i帧的位置信息
+        Eigen::Quaterniond Qi(parameters[0][6], parameters[0][3], parameters[0][4], parameters[0][5]);//Qi 是i帧的四元数旋转
 
-        Eigen::Vector3d Vi(parameters[1][0], parameters[1][1], parameters[1][2]);
-        Eigen::Vector3d Bai(parameters[1][3], parameters[1][4], parameters[1][5]);
-        Eigen::Vector3d Bgi(parameters[1][6], parameters[1][7], parameters[1][8]);
+        //parameters[1] 对应i帧的速度和IMU偏置。
+        Eigen::Vector3d Vi(parameters[1][0], parameters[1][1], parameters[1][2]);//Vi 是i帧的速度向量
+        Eigen::Vector3d Bai(parameters[1][3], parameters[1][4], parameters[1][5]);//Bai 是加速度计的偏置
+        Eigen::Vector3d Bgi(parameters[1][6], parameters[1][7], parameters[1][8]);//Bgi 是陀螺仪的偏置
 
-        Eigen::Vector3d Pj(parameters[2][0], parameters[2][1], parameters[2][2]);
-        Eigen::Quaterniond Qj(parameters[2][6], parameters[2][3], parameters[2][4], parameters[2][5]);
+        //parameters[2] 对应j帧的位姿（位置和旋转）。
+        Eigen::Vector3d Pj(parameters[2][0], parameters[2][1], parameters[2][2]);//Pj 是j帧的位置信息
+        Eigen::Quaterniond Qj(parameters[2][6], parameters[2][3], parameters[2][4], parameters[2][5]);//Qj 是j帧的旋转四元数
 
-        Eigen::Vector3d Vj(parameters[3][0], parameters[3][1], parameters[3][2]);
-        Eigen::Vector3d Baj(parameters[3][3], parameters[3][4], parameters[3][5]);
-        Eigen::Vector3d Bgj(parameters[3][6], parameters[3][7], parameters[3][8]);
+        //parameters[3] 对应j帧的速度和IMU偏置。
+        Eigen::Vector3d Vj(parameters[3][0], parameters[3][1], parameters[3][2]);//Vj 是j帧的速度向量
+        Eigen::Vector3d Baj(parameters[3][3], parameters[3][4], parameters[3][5]);//Baj是j帧的加速度计
+        Eigen::Vector3d Bgj(parameters[3][6], parameters[3][7], parameters[3][8]);//Bgj是j帧的陀螺仪偏置
 
 //Eigen::Matrix<double, 15, 15> Fd;
 //Eigen::Matrix<double, 15, 12> Gd;
 
-//Eigen::Vector3d pPj = Pi + Vi * sum_t - 0.5 * g * sum_t * sum_t + corrected_delta_p;
-//Eigen::Quaterniond pQj = Qi * delta_q;
-//Eigen::Vector3d pVj = Vi - g * sum_t + corrected_delta_v;
+//Eigen::Vector3d pPj = Pi + Vi * sum_t - 0.5 * g * sum_t * sum_t + corrected_delta_p; pPj 是通过i帧的位置，结合IMU预积分数据，预测出的j帧的位置。
+//Eigen::Quaterniond pQj = Qi * delta_q;   pQj 是通过i帧的旋转，结合IMU预积分数据，预测出的j帧的旋转。
+//Eigen::Vector3d pVj = Vi - g * sum_t + corrected_delta_v;  pVj 是通过i帧的速度和IMU数据，预测出的j帧的速度。
 //Eigen::Vector3d pBaj = Bai;
 //Eigen::Vector3d pBgj = Bgi;
 
 //Vi + Qi * delta_v - g * sum_dt = Vj;
 //Qi * delta_q = Qj;
 
-//delta_p = Qi.inverse() * (0.5 * g * sum_dt * sum_dt + Pj - Pi);
-//delta_v = Qi.inverse() * (g * sum_dt + Vj - Vi);
-//delta_q = Qi.inverse() * Qj;
+//delta_p = Qi.inverse() * (0.5 * g * sum_dt * sum_dt + Pj - Pi); 位移残差=i帧旋转矩阵的逆，乘以两个帧之间的位移差。
+//delta_v = Qi.inverse() * (g * sum_dt + Vj - Vi); 速度残差=i帧旋转矩阵的逆，乘以速度差。
+//delta_q = Qi.inverse() * Qj; 旋转残差=i帧的旋转四元数的逆，乘以j帧的旋转四元数。
 
 #if 0
+        //判断偏置变化并重新计算预积分
         if ((Bai - pre_integration->linearized_ba).norm() > 0.10 ||
             (Bgi - pre_integration->linearized_bg).norm() > 0.01)
         {
@@ -98,11 +107,11 @@ class IMUFactor : public ceres::SizedCostFunction<15, 7, 9, 7, 9>
                 Eigen::Map<Eigen::Matrix<double, 15, 7, Eigen::RowMajor>> jacobian_pose_i(jacobians[0]);
                 jacobian_pose_i.setZero();
 
-                jacobian_pose_i.block<3, 3>(O_P, O_P) = -Qi.inverse().toRotationMatrix();
-                jacobian_pose_i.block<3, 3>(O_P, O_R) = Utility::skewSymmetric(Qi.inverse() * (0.5 * G * sum_dt * sum_dt + Pj - Pi - Vi * sum_dt));
+                jacobian_pose_i.block<3, 3>(O_P, O_P) = -Qi.inverse().toRotationMatrix();//位置对位置的雅可比
+                jacobian_pose_i.block<3, 3>(O_P, O_R) = Utility::skewSymmetric(Qi.inverse() * (0.5 * G * sum_dt * sum_dt + Pj - Pi - Vi * sum_dt));//位置对旋转的雅可比
 
 #if 0
-            jacobian_pose_i.block<3, 3>(O_R, O_R) = -(Qj.inverse() * Qi).toRotationMatrix();
+                jacobian_pose_i.block<3, 3>(O_R, O_R) = -(Qj.inverse() * Qi).toRotationMatrix();
 #else
                 Eigen::Quaterniond corrected_delta_q = pre_integration->delta_q * Utility::deltaQ(dq_dbg * (Bgi - pre_integration->linearized_bg));
                 jacobian_pose_i.block<3, 3>(O_R, O_R) = -(Utility::Qleft(Qj.inverse() * Qi) * Utility::Qright(corrected_delta_q)).bottomRightCorner<3, 3>();
